fb27aa7c91dceae026c294fb207278e01d783f7a
JuanSierra <jksierra@gmail.com>
2023-08-13 11:14:53 +0100

    Add graphql client and query example

--- Commands/RequestCommand.cs
+++ Commands/RequestCommand.cs
@@ -1,7 +1,15 @@
-﻿using CliFx;
+﻿using AutoRequestStore.CommonSchema;
+using CliFx;
 using CliFx.Attributes;
 using CliFx.Infrastructure;
+using CSJsonDB;
+using GraphQL;
+using GraphQL.Client.Abstractions;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Text.Json;
 
 namespace AutoRequestStore.Commands
 {
@@ -21,10 +29,54 @@ namespace AutoRequestStore.Commands
         [CommandOption("interval", 'i', Description = "Interval in milliseconds.")]
         public double Interval { get; init; } = 1000;
 
+        private string baseQuery2 = @"
+            query allPeople {
+                allPeople(first: 10) {
+                    edges {
+                        node {
+                            name
+                            gender
+                            }
+                    }
+                }
+            }";
+        private readonly IGraphQLClient _client;
+
+        public RequestCommand(IGraphQLClient client)
+        {
+            _client = client;
+        }
 
         public async ValueTask ExecuteAsync(IConsole console)
         {
+            List<ExpandoObject> resultList = new List<ExpandoObject>();
 
+            var rootNode = Converter.ParseNodesFromQuery(baseQuery2);
+            var schema = Converter.BuildSchema(rootNode);
+
+
+            var queryRequest = new GraphQLRequest(baseQuery2);
+            var graphQLResponse = await _client.SendQueryAsync<JsonElement>(queryRequest);
+            resultList = Converter.GetResultList(graphQLResponse.Data, schema["children"].AsArray());
+
+            // SAVE TO JSON FILE
+            console.Output.WriteLine("START SAVING...");
+            console.Output.WriteLine(resultList.ToJsonString());
+
+            var db = JsonDB.Load("data.db");
+
+            //var i = 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            foreach (var item in resultList)
+            {
+                db.Add(rootNode.Name.StringValue, item);
+                db = JsonDB.Load("data.db");
+            }
+
+            sw.Stop();
+            console.Output.WriteLine(sw.ElapsedMilliseconds);
         }
     }
 }
